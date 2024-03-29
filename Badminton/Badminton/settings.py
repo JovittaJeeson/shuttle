07f9bb4bdea835cea1b77999a2017fbaa0902792
JovittaJeeson <77148041+JovittaJeeson@users.noreply.github.com>
2024-03-29 21:22:06 +0530

    Update settings.py

--- Badminton/Badminton/settings.py
+++ Badminton/Badminton/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-v7n=k9+)pi1^v99evwll#5h^y1h39rd&8ix#30*l22f913vbtj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# settings.py
+
+ALLOWED_HOSTS = ['yourdomain.com', 'localhost', '127.0.0.1', '[::1]', '13.60.31.83']
 
 # Application definition
 
