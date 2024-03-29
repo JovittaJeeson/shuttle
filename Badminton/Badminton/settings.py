5605571e08b940b300ffd6ada07256a285c00dcc
JovittaJeeson <77148041+JovittaJeeson@users.noreply.github.com>
2024-03-29 20:46:37 +0530

    Update settings.py

--- Badminton/Badminton/settings.py
+++ Badminton/Badminton/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-v7n=k9+)pi1^v99evwll#5h^y1h39rd&8ix#30*l22f913vbtj
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['yourdomain.com', '13.60.31.83', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
